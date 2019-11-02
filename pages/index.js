85d0a88276b82e8c374d17e4ebb7433e9bec3129
nullbus <ppparkje@gmail.com>
2019-11-02 17:45:39 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+export default () => (
+    <div>
+        <h1>nullbus</h1>
+        <Link href="https://github.com/nullbus"><a>Github</a></Link>
+        <br />
+        <Link href="https://blog.nullbus.net"><a>Blog</a></Link>
+    </div>
+);
