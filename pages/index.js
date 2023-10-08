f14b8bbeebfbc90d78a8e4437011e93aca61cb79
Jaemin Park <ppparkje@gmail.com>
2023-10-08 23:35:31 +0900

    add bullet

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import Link from 'next/link';
 export default () => (
     <div>
         <h1>nullbus</h1>
-        <Link href="https://github.com/nullbus"><a>Github</a></Link>
-        <br />
-        <Link href="https://blog.nullbus.net"><a>Blog</a></Link>
+        <ul>
+            <li><a href="https://github.com/nullbus">Github</a></li>
+            <li><a href="https://blog.nullbus.net">Blog</a></li>
+        </ul>
     </div>
 );
